04d1b854432800315e70449b8095605ccd85683d
Vinicius Dias <carlosv775@gmail.com>
2020-10-24 16:49:59 -0300

    Vídeo 3.2

--- io-nb.php
+++ io-nb.php
@@ -1,11 +1,11 @@
 <?php
 
 $streamList = [
-    fopen('http://localhost:8080/http-server.php', 'r'),
+    stream_socket_client('tcp://localhost:8080'),
     fopen('arquivo.txt', 'r'),
     fopen('arquivo2.txt', 'r'),
 ];
-
+fwrite($streamList[0], 'GET /http-server.php HTTP/1.1' . PHP_EOL . PHP_EOL);
 foreach ($streamList as $stream) {
     stream_set_blocking($stream, false);
 }
@@ -19,7 +19,13 @@ do {
     }
 
     foreach ($copyReadStream as $key => $stream) {
-        echo fgets($stream);
+        $conteudo = stream_get_contents($stream);
+        $posicaoFimHttp = strpos($conteudo, "\r\n\r\n");
+        if ($posicaoFimHttp !== false) {
+            echo substr($conteudo, $posicaoFimHttp + 4);
+        } else {
+            echo $conteudo;
+        }
         unset($streamList[$key]);
     }
 } while(!empty($streamList));
